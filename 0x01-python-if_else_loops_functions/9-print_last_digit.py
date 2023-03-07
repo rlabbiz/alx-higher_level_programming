3e135ee4ca8e4e0c53ec26f7122baee70d132b71
rlabbiz <redalabbiz@gmail.com>
2023-03-07 13:30:09 -0800

    second push

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        c = (number * -1) % 10
-    else:
-        c = number % 10
-    return c
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
