ec0dd0bec6db603488860e057b65d787bb340352
rlabbiz <redalabbiz@gmail.com>
2023-03-07 13:15:27 -0800

    adding negative check

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-n = number % 10
+if number < 0:
+    n = (number * -1) % 10
+    n *= -1
+else:
+    n = number % 10
 print(f"Last digit of {number} is {n}", end=" ")
 if n > 5:
     print("and is greater than 5")
