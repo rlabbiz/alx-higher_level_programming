b9e99723577cf477c96c62cb44d2ce48cab7b371
rlabbiz <redalabbiz@gmail.com>
2023-03-09 12:55:27 -0800

    no change

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """ print the sum of a and b """
     import add_0 as _add
+
     a = 1
     b = 2
-    print('{} + {} = {}'.format(a, b, _add.add(a, b)))
 
+    print('{} + {} = {}'.format(a, b, _add.add(a, b)))
