a629627251db0a3c34dd462e4ae5987a8c1a20a9
rlabbiz <redalabbiz@gmail.com>
2023-03-09 12:46:20 -0800

    third push

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """print the addition and subtraction and multiplication and division of a and d """
     from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
+
     print('{} + {} = {}'.format(a, b, cal.add(a, b)))
     print('{} - {} = {}'.format(a, b, cal.sub(a, b)))
     print('{} * {} = {}'.format(a, b, cal.mul(a, b)))
