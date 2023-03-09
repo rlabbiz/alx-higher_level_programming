6462486e2f7e4a3abf3569231db4d6dc331e2628
rlabbiz <redalabbiz@gmail.com>
2023-03-09 13:22:13 -0800

    no chenge

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """ print the number of argrument follwed by value of if """
+    """ print the number of argrument follwed by value of it"""
     import sys
 
     count = len(sys.argv) - 1
-
     if count == 0:
         print('0 arguments.')
     else:
-        print('{} arguments:'.format(count))
+        if count == 1:
+            print('1 argument:')
+        else:
+            print('{} arguments:'.format(count))
         for i in range(1, count + 1):
             print('{}: {}'.format(i, sys.argv[i]))
