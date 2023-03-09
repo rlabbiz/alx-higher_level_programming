1684233f51c791ef4d94b582e97ecfca87ea6eb0
rlabbiz <redalabbiz@gmail.com>
2023-03-09 12:29:11 -0800

    add __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as _add
-a = 1
-b = 2
-print('{} + {} = {}'.format(a, b, _add.add(a, b)))
+if __name__ == "__main__":
+    import add_0 as _add
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, _add.add(a, b)))
 
