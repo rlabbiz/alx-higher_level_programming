4523dc3855fabf72918b280c4075cd7b7ed1852e
rlabbiz <redalabbiz@gmail.com>
2023-03-11 07:34:05 -0800

    third push

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
+    if  0 <= idx < len(my_list):
         my_list[idx] = element
+    else:
+        return my_list
