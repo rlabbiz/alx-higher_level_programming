65abd169659067f9025621cd47083cdab82176ca
rlabbiz <redalabbiz@gmail.com>
2023-03-11 07:25:11 -0800

    git push

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-""" function retrieves an element from a list that given as second argument """
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return None
+        return "None"
     else:
         return my_list.index(idx)
