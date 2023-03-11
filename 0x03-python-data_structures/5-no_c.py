26f02f08ca4d2a7a55d3800346a49c249b1e950b
rlabbiz <redalabbiz@gmail.com>
2023-03-11 11:08:52 -0800

    last push

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_str = ''
+    new_str = ""
     for i in my_string:
-        if i != 'c' and i != 'C':
+        if i is not 'c' and i is not 'C':
             new_str += i
     return (new_str)
